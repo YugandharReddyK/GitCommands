139d4067c5252701c489b7b7fbbac8eb274043e1
Yugandhar Reddy <Yugandhar.Reddy@halliburton.com>
2023-03-20 11:03:14 +0530

    Yugandhar - Added application.

---
+++ GitConsole/GitConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
